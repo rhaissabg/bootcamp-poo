da982c204032d74b1e39c33ad95684659f749022
Rhaíssa Lima <rhaissasbgl@gmail.com>
2024-08-05 17:18:49 -0300

    implementada as regras de negócio

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -9,7 +10,7 @@ public class Dev {
 	private String nome;
 	private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
 	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
-	
+
 	public String getNome() {
 		return nome;
 	}
@@ -53,15 +54,22 @@ public class Dev {
 	}
 
 	public void inscreverBootcamp(Bootcamp bootcamp) {
-		
+		this.conteudosInscritos.addAll(bootcamp.getConteudos());
+		bootcamp.getDevsInscritos().add(this);
 	}
-	
+
 	public void progredir() {
-		
+		Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+		if (conteudo.isPresent()) {
+			this.conteudosConcluidos.add(conteudo.get());
+			this.conteudosInscritos.remove(conteudo.get());
+		} else {
+			System.err.println("Você não está matriculado em nenhum conteúdo!");
+		}
 	}
-	
-	public void calcularTotalXp() {
-		
+
+	public double calcularTotalXp() {
+		return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
 	}
-	
+
 }
