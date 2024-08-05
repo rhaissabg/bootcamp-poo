910547a41b4fb571e6a7a125b5192f5d8dfa65e6
Rhaíssa Lima <rhaissasbgl@gmail.com>
2024-08-05 15:04:26 -0300

    criada a classe pai conteudo

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,40 +1,29 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo {
 
-	private String titulo;
-	private String descricao;
 	private int cargaHoraria;
-	
-	public Curso() {}
-	
-	public String getTitulo() {
-		return titulo;
+
+	public Curso() {
 	}
-	
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	
-	public String getDescricao() {
-		return descricao;
-	}
-	
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	
+
 	public int getCargaHoraria() {
 		return cargaHoraria;
 	}
-	
+
 	public void setCargaHoraria(int cargaHoraria) {
 		this.cargaHoraria = cargaHoraria;
 	}
 
 	@Override
-	public String toString() {
-		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+	public double calcularXp() {
+		return XP_PADRAO * getCargaHoraria();
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria
+				+ "]";
+	}
+
 }
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,28 +2,11 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo {
 
-	private String titulo;
-	private String descricao;
 	private LocalDate data;
-	
-	public Mentoria() {}
 
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	public Mentoria() {
 	}
 
 	public LocalDate getData() {
@@ -35,8 +18,13 @@ public class Mentoria {
 	}
 
 	@Override
+	public double calcularXp() {
+		return XP_PADRAO + 20;
+	}
+
+	@Override
 	public String toString() {
-		return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+		return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + data + "]";
 	}
 
 }
